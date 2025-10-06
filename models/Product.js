8e832aa3f858f5ceac8623f44c701bbcc44f5b56
Pratham Singhal <160416168+prathamsinghal000@users.noreply.github.com>
2025-10-06 12:50:56 +0530

    Add files via upload

---
+++ models/Product.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Product name is required']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Product price is required'],
+        min: [0, 'Price cannot be negative']
+    },
+    category: {
+        type: String,
+        required: [true, 'Product category is required']
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Product', productSchema);
